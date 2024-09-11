e08e3371f85fdfc1e4f9e25a38d93ab26c21ce88
muhammadaziz <aziz.nigmatjonov7@gmail.com>
2024-09-11 16:07:28 +0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-echo "Switching to branch master"
-git checkout master
+# echo "Switching to branch master"
+# git checkout master
 
-echo "Building app..."
-npm run build
+# echo "Building app..."
+# npm run build
 
-ssh-keygen -R 10.40.15.10
+# ssh-keygen -R 10.40.15.10
 
-echo "Deploying files to server..."
-scp -r dist/* ladmin@10.40.12.40:/var/www/mfdashboard/
+# echo "Deploying files to server..."
+# scp -r dist/* ladmin@10.40.12.40:/var/www/mfdashboard/
 
-echo "Done!"
+# echo "Done!"
