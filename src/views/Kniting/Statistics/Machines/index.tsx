a29158e81b5fb07dc471de1938b99398d4778bf7
Нигматжонов Азизжон Нодиржон <a.nigmatzhonov@praktik.loc>
2024-10-14 12:10:50 +0500

    fixed

--- src/views/Kniting/Statistics/Machines/index.tsx
+++ src/views/Kniting/Statistics/Machines/index.tsx
@@ -2,29 +2,29 @@ import { Header } from "../../../../components/UI/Header";
 import CTabs from "../../../../components/CElements/CTab";
 import { TabList } from "./Logic";
 import { useState } from "react";
-import { MachineList } from "./Pages/List";
-import { MachineStatus } from "./Pages/Status";
-import { MachinePlans } from "./Pages/Plan";
-import { MachineDefects } from "./Pages/Defects";
+// import { MachineList } from "./Pages/List";
+// import { MachineStatus } from "./Pages/Status";
+// import { MachinePlans } from "./Pages/Plan";
+// import { MachineDefects } from "./Pages/Defects";
 // import { CPeriodPicker } from "../../../../components/CElements/CPeriodPicker";
 
 const KnitingdAnalytics = () => {
   const [currentTab, setCurrentTab] = useState({ name: "", id: "list" });
 
-  const CurrentPage = (tab: string) => {
-    switch (tab) {
-      case "list":
-        return <MachineList />;
-      case "status":
-        return <MachineStatus />;
-      case "plan":
-        return <MachinePlans />;
-      case "defects":
-        return <MachineDefects />;
-      default:
-        return <MachineList />;
-    }
-  };
+  // const CurrentPage = (tab: string) => {
+  //   switch (tab) {
+  //     case "list":
+  //       return <MachineList />;
+  //     case "status":
+  //       return <MachineStatus />;
+  //     case "plan":
+  //       return <MachinePlans />;
+  //     case "defects":
+  //       return <MachineDefects />;
+  //     default:
+  //       return <MachineList />;
+  //   }
+  // };
 
   return (
     <div>
@@ -42,7 +42,7 @@ const KnitingdAnalytics = () => {
         {/* <CPeriodPicker handleValue={() => {}} placeholder="Время" /> */}
       </Header>
 
-      <div className="container">{CurrentPage(currentTab?.id)}</div>
+      {/* <div className="container">{CurrentPage(currentTab?.id)}</div> */}
     </div>
   );
 };
