83bb67479f907e6dca36f551ab83326a437d4690
Нигматжонов Азизжон Нодиржон <a.nigmatzhonov@praktik.loc>
2024-10-17 12:09:34 +0500

    added time format

--- src/hooks/useCalucaleTime/index.tsx
+++ src/hooks/useCalucaleTime/index.tsx
@@ -1,25 +1,18 @@
 export const useCalucaleTime = () => {
   const GetTime: any = (old_time: string) => {
     const startTime: any = new Date(old_time);
-
-    // Get the current time
     const currentTime: any = new Date();
-
-    // Calculate the time difference in milliseconds
     const timeDifference = currentTime - startTime;
-
-    // Convert the difference to hours, minutes, and seconds
     const hours = Math.floor(timeDifference / (1000 * 60 * 60));
     const minutes = Math.floor(
       (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
     );
-    // const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const formattedHours = String(hours).padStart(2, "0");
+    const formattedMinutes = String(minutes).padStart(2, "0");
 
-    // Format the result as h:m:s
-    const timeAgo = `${hours}:${minutes}`;
+    const timeAgo = `${formattedHours}:${formattedMinutes}`;
 
     return timeAgo;
   };
-
   return { GetTime };
 };
