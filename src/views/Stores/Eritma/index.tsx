119afb5a2696e811421b2ab6a65021e2ec21cd4c
Нигматжонов Азизжон Нодиржон <a.nigmatzhonov@praktik.loc>
2025-03-04 17:19:57 +0500

    added: new table

--- src/views/Stores/Eritma/index.tsx
+++ src/views/Stores/Eritma/index.tsx
@@ -3,10 +3,19 @@ import { breadCrumbs, TableData } from "./Logic";
 import CBreadcrumbs from "../../../components/CElements/CBreadcrumbs";
 import CNewTable from "../../../components/CElements/CNewTable";
 import { Header } from "../../../components/UI/Header";
+import useCQuery from "../../../hooks/useCQuery";
 
 export const EritmaPage = () => {
   const { headColumns, bodyColumns, handleActions } = TableData();
   const [filterParams, setFilterParams]: any = useState({});
+
+  const { data: uruns, isLoading } = useCQuery({
+    key: `GET_URUN_LIST`,
+    endpoint: `http://10.40.14.193:8000/urun/?skip=0&limit=100`,
+    params: {},
+  });
+
+  console.log("uruns", uruns);
 
   return (
     <>
